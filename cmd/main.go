ce6f65229741309025e9511a0d2f8c60e99e9222
M Price <zaptross@outlook.com>
2023-10-27 22:52:05 +1000

    fix: update logging to account for page title length

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strings"
 
+	"github.com/samber/lo"
 	g "github.com/zaptross/gorgeous"
 	c "github.com/zaptross/portfoligo/internal/components"
 	"github.com/zaptross/portfoligo/internal/pages"
+	"github.com/zaptross/portfoligo/internal/types"
 )
 
 func main() {
@@ -20,15 +23,24 @@ func main() {
 		Include:  true,
 		Selector: ":root",
 		Props: g.CSSProps{
-			"font-size": "20px",
+			"font-size":   "20px",
 			"font-family": "'Lato', sans-serif",
 		},
+	})
+
+	displayTab := 2
+	longestName := 0
+	lo.ForEach(pages.GetAllPages(), func(page types.PageDetails, _ int) {
+		if len(page.Title) > longestName {
+			longestName = len(page.Title)
+		}
 	})
 
 	println("===  Rendering pages\t===")
 	for _, pages := range pages.GetAllPagesByTypes() {
 		for _, page := range pages {
-			println(fmt.Sprintf("%s%s@ %s", page.Title, strings.Repeat(" ", 2*8-len(page.Title)), page.GetRelativeURL()))
+			spacing := int(math.Ceil(float64(longestName)/float64(displayTab)))*displayTab-len(page.Title)
+			println(fmt.Sprintf("%s%s-> %s", page.Title, strings.Repeat(" ", spacing), page.GetRelativeURL()))
 			rendered := g.RenderStatic(
 				g.Document(
 					c.Head(page),
