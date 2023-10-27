949b9bf30d8e0a900c88b06a103f8a156e7d6376
M Price <zaptross@outlook.com>
2023-10-27 18:05:05 +1000

    feat(about): add skeleton about page, testing root

---
+++ internal/pages/aboutPage.go
@@ -0,0 +1,27 @@
+package pages
+
+import (
+	g "github.com/zaptross/gorgeous"
+	c "github.com/zaptross/portfoligo/internal/components"
+	"github.com/zaptross/portfoligo/internal/types"
+)
+
+var (
+	_ = registerPage(types.PageDetails{
+		Title:       "About",
+		Description: "About Me",
+		Slug:        "about",
+		Type:        TYPE_ROOT,
+		Content: g.Div(g.EB{
+			Children: []*g.HTMLElement{
+				g.H1(g.EB{
+					Text: "Matthew Price",
+				}),
+				c.P(g.EB{
+					Text: "I am a full stack polyglot developer.",
+				}),
+				c.LinkRel("NoRhythm", "/project/no-rhythm"),
+			}},
+		),
+	})
+)
