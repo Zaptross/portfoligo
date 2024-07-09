de3dfc8adc15379d83dfb3ee188cf54a7e90cfcf
M Price <zaptross@outlook.com>
2024-07-10 00:34:03 +1000

    refactor(theme): split struct into substructs for instantiation

--- internal/types/theme.go
+++ internal/types/theme.go
@@ -3,52 +3,68 @@ package types
 import "fmt"
 
 type Theme struct {
-	space  int
-	Colors struct {
-		Text struct {
-			Primary   string
-			Secondary string
-			Link      string
-			Heading   string
-			Hover     string
-		}
-		Background struct {
-			Primary   string
-			Secondary string
-			Accent    string
-		}
-		Error   string
-		Warning string
-		Info    string
-		Success string
-	}
-	Fonts struct {
-		Body      string
-		Heading   string
-		Monospace string
-	}
-	FontWeights struct {
-		Body    string
-		Heading string
-		Bold    string
-	}
-	Borders struct {
-		None  string
-		Thin  string
-		Thick string
-	}
-	Radii struct {
-		None    string
-		Small   string
-		Default string
-		Large   string
-	}
-	ZIndices struct {
-		Auto    string
-		Overlay string
-		Drawers string
-		Modals  string
-	}
+	space    int
+	Colors   Colors
+	Fonts    Fonts
+	Weights  FontWeights
+	Borders  Borders
+	Radii    Radii
+	ZIndices ZIndices
+}
+
+type Colors struct {
+	Text       TextColors
+	Background BackgroundColors
+	Error      string
+	Warning    string
+	Info       string
+	Success    string
+}
+
+type TextColors struct {
+	Primary   string
+	Secondary string
+	Link      string
+	Heading   string
+	Hover     string
+}
+
+type BackgroundColors struct {
+	Primary   string
+	Secondary string
+	Accent    string
+}
+
+type Fonts struct {
+	Body      string
+	Heading   string
+	Monospace string
+}
+
+type FontWeights struct {
+	Body    string
+	Heading string
+	Bold    string
+}
+
+type Borders struct {
+	None  string
+	Thin  string
+	Thick string
+}
+
+type Radii struct {
+	None    string
+	Small   string
+	Default string
+	Large   string
+}
+
+type ZIndices struct {
+	Auto    string
+	Overlay string
+	Drawers string
+	Modals  string
 }
 
 func (t *Theme) Spacing(i int) string {
